5434eebdd5e23f7dde894fa37e65e956be4ca31e
Sebastián Mancilla <smancill@jlab.org>
2015-05-11 17:55:20 -0300

    Add EngineCallback interface

---
+++ src/org/jlab/clara/base/EngineCallback.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015. Jefferson Lab, CLARA framework (JLAB). All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for educational, research, and not-for-profit purposes,
+ * without fee and without a signed licensing agreement.
+ *
+ * Author Vardan Gyurjyan
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+ * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+ * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+package org.jlab.clara.base;
+
+import org.jlab.clara.engine.EngineData;
+
+/**
+ * An interface to handle the reports published by Clara services.
+ */
+public interface EngineCallback {
+
+    /**
+     * Receives and process the data that a service has published.
+     * The data can contain the actual result of the service and/or status
+     * information about the execution that generated the data.
+     *
+     * @param data the published data
+     */
+    void callback(EngineData data);
+}
