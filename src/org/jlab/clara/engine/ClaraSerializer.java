9975ee5801a0220db2edbe21146987e200bf20dd
Sebastián Mancilla <smancill@jlab.org>
2015-08-13 12:27:17 -0400

    Add custom defined serialization interface
    Allow services to define their own mime-types and provide the methods to
    serialize the data.

---
+++ src/org/jlab/clara/engine/ClaraSerializer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015. Jefferson Lab, CLARA framework (JLAB). All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for educational, research, and not-for-profit purposes,
+ * without fee and without a signed licensing agreement.
+ *
+ * Author Vardan Gyurjyan
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+ * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+ * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+package org.jlab.clara.engine;
+
+import java.nio.ByteBuffer;
+
+import org.jlab.clara.base.error.ClaraException;
+
+/**
+ * Provides the custom serialization methods to send user defined data through
+ * the network.
+ */
+public interface ClaraSerializer {
+
+    /**
+     * Serializes the user object into a byte buffer and returns it.
+     *
+     * @param data the user object stored on the {@link EngineData}
+     * @throws ClaraException if the data could not be serialized
+     */
+    ByteBuffer write(Object data) throws ClaraException;
+
+    /**
+     * Deserializes the byte buffer into the user object and returns it.
+     *
+     * @param data the serialized data
+     * @throws ClaraException if the data could not be deserialized
+     */
+    Object read(ByteBuffer buffer) throws ClaraException;
+}
