bea07033e7081bf5d32e4c7fb59068c7163505db
Sebastián Mancilla <smancill@jlab.org>
2015-12-16 16:18:48 -0300

    DPE: fix publication of reports
    Reports are being dropped by xMsg, because current connection manager
    always creates a new connection for every connect request (needs fix on
    xMsg).
    
    Use a single connection to send reports.

--- src/org/jlab/clara/sys/Dpe.java
+++ src/org/jlab/clara/sys/Dpe.java
@@ -37,6 +37,7 @@ import org.jlab.coda.xmsg.core.xMsgSubscription;
 import org.jlab.coda.xmsg.core.xMsgTopic;
 import org.jlab.coda.xmsg.core.xMsgUtil;
 import org.jlab.coda.xmsg.excp.xMsgException;
+import org.jlab.coda.xmsg.net.xMsgConnection;
 import org.jlab.coda.xmsg.net.xMsgProxyAddress;
 import org.jlab.coda.xmsg.xsys.xMsgProxy;
 import org.zeromq.ZContext;
@@ -245,6 +246,9 @@ public class Dpe extends ClaraBase {
             xMsgTopic dpeReportTopic = ClaraUtil.buildTopic(CConstants.DPE_ALIVE,
                                                             getMe().getCanonicalName());
 
+            xMsgConnection con = connect(getFrontEnd().getProxyAddress());
+            xMsgUtil.sleep(100);
+
             while (isReporting.get()) {
 
                 myReport.setMemoryUsage(ClaraUtil.getMemoryUsage());
@@ -254,9 +258,10 @@ public class Dpe extends ClaraBase {
                 String jsonData = myReportBuilder.generateReport(myReport);
 
                 xMsgMessage msg = new xMsgMessage(dpeReportTopic, jsonData);
-                send(getFrontEnd(), msg);
+                send(con, msg);
                 xMsgUtil.sleep(reportWait);
             }
+            release(con);
         } catch (IOException | xMsgException | MalformedObjectNameException |
                 ReflectionException | InstanceNotFoundException e) {
             e.printStackTrace();
