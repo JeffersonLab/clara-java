1dfe3960fba2a3729769172d7dcfd1b1142393d9
Vardan Gyurjyan <gurjyan@jlab.org>
2016-09-26 17:10:42 -0400

    factorial service engine

---
+++ src/org/jlab/clara/examples/engines/Factorial.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015. Jefferson Lab, CLARA framework (JLAB). All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for educational, research, and not-for-profit purposes,
+ * without fee and without a signed licensing agreement.
+ *
+ * Contact Vardan Gyurjyan
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+ * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+ * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+package org.jlab.clara.examples.engines;
+
+import org.jlab.clara.engine.Engine;
+import org.jlab.clara.engine.EngineData;
+import org.jlab.clara.engine.EngineDataType;
+
+import java.util.Set;
+
+/**
+ * Factorial calculating service engine
+ * Created by gurjyan on 9/26/16.
+ */
+public class Factorial implements Engine {
+
+    private double fact(int n)
+    {
+        double result;
+
+        if(n==1)
+            return 1;
+
+        result = fact(n-1) * n;
+        return result;
+    }
+
+    @Override
+    public EngineData configure(EngineData input) {
+        return null;
+    }
+
+    @Override
+    public EngineData execute(EngineData input) {
+        fact(7777);
+        return input;
+    }
+
+    @Override
+    public EngineData executeGroup(Set<EngineData> inputs) {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getInputDataTypes() {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getOutputDataTypes() {
+        return null;
+    }
+
+    @Override
+    public Set<String> getStates() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
