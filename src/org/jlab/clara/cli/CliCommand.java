84108b867848cf5997fed17ec76c520486e37a43
gurjyan <gurjyan@jlab.org>
2017-01-11 12:31:27 -0500

    - script updates. - start of cli package

---
+++ src/org/jlab/clara/cli/CliCommand.java
@@ -0,0 +1,15 @@
+package org.jlab.clara.cli;
+
+/**
+ * Class description here....
+ * <p>
+ *
+ * @author gurjyan
+ *         Date 1/10/17
+ * @version 3.x
+ */
+public class CliCommand <A> {
+    public CliCommand(final String name, final Fn<String, A> parseFunction){
+
+    }
+}
---
+++ src/org/jlab/clara/cli/Fn.java
@@ -0,0 +1,13 @@
+package org.jlab.clara.cli;
+
+/**
+ * Class description here....
+ * <p>
+ *
+ * @author gurjyan
+ *         Date 1/10/17
+ * @version 3.x
+ */
+public interface Fn<I,R> {
+    R apply(I input);
+}
