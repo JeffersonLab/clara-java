36569f2a5ef0f09d8cd5819ac9d43bb1b5466db0
Sebastián Mancilla <smancill@jlab.org>
2017-02-10 12:14:12 -0300

    shell: add helper to run an interactive external process

---
+++ src/org/jlab/clara/cli/CommandUtils.java
@@ -0,0 +1,48 @@
+/*
+ *   Copyright (c) 2017.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ *
+ *   IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ *   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ *   OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ *   BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ *   PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ *   MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *   This software was developed under the United States Government license.
+ *   For more information contact author at gurjyan@jlab.org
+ *   Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
+
+package org.jlab.clara.cli;
+
+import java.io.IOException;
+import java.util.Optional;
+
+final class CommandUtils {
+
+    private CommandUtils() { }
+
+    public static void runProcess(String... command) {
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        try {
+            Process process = builder.start();
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                process.destroy();
+                Thread.currentThread().interrupt();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
