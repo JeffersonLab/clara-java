a5c46552be47b668aefd94271ddcc18275736293
Sebastián Mancilla <smancill@jlab.org>
2017-02-25 22:14:43 -0300

    std: add base service class

---
+++ src/org/jlab/clara/std/services/AbstractService.java
@@ -0,0 +1,80 @@
+/*
+ *   Copyright (c) 2017.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ *
+ *   IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ *   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ *   OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ *   BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ *   PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ *   MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *   This software was developed under the United States Government license.
+ *   For more information contact author at gurjyan@jlab.org
+ *   Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
+
+package org.jlab.clara.std.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jlab.clara.base.logging.Logger;
+import org.jlab.clara.base.logging.LoggerFactory;
+import org.jlab.clara.engine.Engine;
+import org.jlab.clara.engine.EngineData;
+import org.jlab.clara.engine.EngineSpecification;
+
+/**
+ * An abstract writer service that writes all received events into the
+ * configured output file.
+ */
+public abstract class AbstractService implements Engine {
+
+    // Experimental specification file
+    private final EngineSpecification info = new EngineSpecification(this.getClass());
+
+    /** This service logger. */
+    protected final Logger logger = new LoggerFactory().getLogger(info.name());
+
+
+    @Override
+    public EngineData executeGroup(Set<EngineData> inputs) {
+        return null;
+    }
+
+    /**
+     * Returns the name of the engine.
+     *
+     * @return the engine name
+     */
+    public String getName() {
+        return info.name();
+    }
+
+    @Override
+    public Set<String> getStates() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getDescription() {
+        return info.description();
+    }
+
+    @Override
+    public String getVersion() {
+        return info.version();
+    }
+
+    @Override
+    public String getAuthor() {
+        return String.format("%s  <%s>", info.author(), info.email());
+    }
+}
