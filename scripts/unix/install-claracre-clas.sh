db35c81f9c49591915952f9171bb629f907202f5
gurjyan <gurjyan@jlab.org>
2017-07-20 14:00:31 -0400

    clas install script and empty service.

--- scripts/unix/install-claracre-clas.sh
+++ scripts/unix/install-claracre-clas.sh
@@ -102,7 +102,7 @@ cd coatjava || exit
 
 cp -r etc "$CLARA_HOME"/plugins/clas12/.
 cp -r bin "$CLARA_HOME"/plugins/clas12/.
-cp -r lib/packages "$CLARA_HOME"/plugins/clas12/lib/
+#cp -r lib/packages "$CLARA_HOME"/plugins/clas12/lib/
 cp -r lib/utils "$CLARA_HOME"/plugins/clas12/lib/
 cp  lib/clas/* "$CLARA_HOME"/plugins/clas12/lib/clas/.
 cp  lib/services/* "$CLARA_HOME"/plugins/clas12/lib/services/.
@@ -126,7 +126,7 @@ rm -rf coatjava
 rm coatjava-$PLUGIN.tar.gz
 
 chmod a+x "$CLARA_HOME"/bin/*
-chmod a+x "$CLARA_HOME"/bin/etc/*
+#chmod a+x "$CLARA_HOME"/bin/etc/*
 
 
 echo "Done!"
---
+++ src/org/jlab/clara/examples/engines/Empty.java
@@ -0,0 +1,98 @@
+/*
+ *   Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ *
+ *   IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ *   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ *   OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ *   BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ *   PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ *   MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *   This software was developed under the United States Government license.
+ *   For more information contact author at gurjyan@jlab.org
+ *   Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
+
+package org.jlab.clara.examples.engines;
+
+import org.jlab.clara.base.ClaraUtil;
+import org.jlab.clara.base.core.ClaraConstants;
+import org.jlab.clara.engine.Engine;
+import org.jlab.clara.engine.EngineData;
+import org.jlab.clara.engine.EngineDataType;
+
+import java.util.Set;
+
+/**
+ * Empty engine
+ *
+ * @author gurjyan
+ * @version 1.x
+ * @since 2/9/17
+ */
+public class Empty implements Engine {
+
+    @Override
+    public EngineData execute(EngineData x) {
+         return x;
+    }
+
+    @Override
+    public EngineData executeGroup(Set<EngineData> x) {
+        System.out.println("E1 engine group execute...");
+        return x.iterator().next();
+    }
+
+    @Override
+    public EngineData configure(EngineData x) {
+        System.out.println("E1 engine configure...");
+        return x;
+    }
+
+    @Override
+    public Set<String> getStates() {
+        return null;
+    }
+
+    @Override
+    public Set<EngineDataType> getInputDataTypes() {
+        return ClaraUtil.buildDataTypes(EngineDataType.STRING);
+    }
+
+    @Override
+    public Set<EngineDataType> getOutputDataTypes() {
+        return ClaraUtil.buildDataTypes(EngineDataType.STRING);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Sample service E1";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Vardan Gyurgyan";
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
