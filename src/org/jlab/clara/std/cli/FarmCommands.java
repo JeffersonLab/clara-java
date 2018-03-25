JeffersonLab/clara-java

commit 958613898d0b8296893fbf86b3e47af7a512fef5

shell: improve quoting of external commands
Do not pass a 'quote' argument to the builder, but auto-detect if the
arguments need quotes or not.

By default only add quotes to arguments that contains spaces,
environment variables or any non-trivial characters. The other option
was to quote everything unconditionally, but that seems unnecessary, and
the output script containing the command ended up with too many quotes.
In any case, the behaviour can be toggled.

Also, when creating an array, the arguments should not be quoted
(they will be handled by the ProcessBuilder), so the 'quote' parameter
was unnecessary.

src/org/jlab/clara/std/cli/FarmCommands.java
@@ -294,7 +294,7 @@ final class FarmCommands {
 
         private String getClaraCommand() {
             Path wrapper = FileUtils.claraPath("lib", "clara", "run-clara");
-            CommandBuilder cmd = new CommandBuilder(wrapper, true);
+            SystemCommandBuilder cmd = new SystemCommandBuilder(wrapper);
 
             cmd.addOption("-i", config.getString(Config.INPUT_DIR));
             cmd.addOption("-o", config.getString(Config.OUTPUT_DIR));

src/org/jlab/clara/std/cli/CommandUtils.java
@@ -164,7 +164,7 @@ public final class CommandUtils {
      * @return the wrapper program that runs the given command
      */
     public static String[] uninterruptibleCommand(String[] command, Path logFile) {
-        CommandBuilder b = new CommandBuilder(commandLogger(), false);
+        SystemCommandBuilder b = new SystemCommandBuilder(commandLogger());
         b.addArgument(logFile);
         Arrays.asList(command).forEach(b::addArgument);
         return b.toArray();

src/org/jlab/clara/std/cli/RunCommand.java
@@ -91,7 +91,7 @@ class RunCommand extends BaseCommand {
 
         private String[] orchestratorCmd(DpeName feName) {
             Path orchestrator = FileUtils.claraPath("bin", "clara-orchestrator");
-            CommandBuilder cmd = new CommandBuilder(orchestrator, false);
+            SystemCommandBuilder cmd = new SystemCommandBuilder(orchestrator);
 
             cmd.addOption("-F");
             cmd.addOption("-f", feName);

src/org/jlab/clara/std/cli/SystemCommandBuilder.java
@@ -25,19 +25,24 @@ package org.jlab.clara.std.cli;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-class CommandBuilder {
+class SystemCommandBuilder {
 
-    private final boolean quote;
+    private static final Pattern NEED_QUOTES = Pattern.compile("[^A-Za-z0-9/._-]");
+
     private final List<String> cmd = new ArrayList<>();
 
-    CommandBuilder(Path program, boolean useQuotes) {
-        this(program.toString(), useQuotes);
+    private boolean quoteAll;
+
+    SystemCommandBuilder(Path program) {
+        this(program.toString());
     }
 
-    CommandBuilder(String program, boolean useQuotes) {
-        quote = useQuotes;
-        cmd.add(mayQuote(program));
+    SystemCommandBuilder(String program) {
+        cmd.add(program);
     }
 
     public void addOption(String option) {
@@ -46,26 +51,33 @@ class CommandBuilder {
 
     public void addOption(String option, Object value) {
         cmd.add(option);
-        cmd.add(mayQuote(value));
+        cmd.add(value.toString());
     }
 
     public void addArgument(Object argument) {
-        cmd.add(mayQuote(argument));
+        cmd.add(argument.toString());
     }
 
-    private String mayQuote(Object value) {
-        if (quote) {
+    public void quoteAll(boolean quote) {
+        quoteAll = quote;
+    }
+
+    private String mayQuote(String value) {
+        Matcher m = NEED_QUOTES.matcher(value);
+        if (m.find() || quoteAll) {
             return "\"" + value + "\"";
         }
-        return value.toString();
+        return value;
     }
 
     public String[] toArray() {
-        return cmd.toArray(new String[0]);
+        return cmd.stream().toArray(String[]::new);
     }
 
     @Override
     public String toString() {
-        return String.join(" ", cmd);
+        return cmd.stream()
+                .map(this::mayQuote)
+                .collect(Collectors.joining(" "));
     }
 }

test/org/jlab/clara/std/cli/SystemCommandBuilderTest.java
@@ -1,0 +1,54 @@
+package org.jlab.clara.std.cli;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SystemCommandBuilderTest {
+
+    private SystemCommandBuilder b;
+
+    @Before
+    public void setUp() {
+        b = new SystemCommandBuilder("${CLARA_HOME}/bin/clara-orchestrator");
+        b.addOption("-t", 10);
+        b.addOption("-i", "$CLAS12DIR/exp/input");
+        b.addArgument("custom services.yml");
+        b.addArgument("data/files.txt");
+    }
+
+    @Test
+    public void outputArrayDoesNotNeedQuotes() throws Exception {
+        assertThat(b.toArray(), is(new String[]{
+                "${CLARA_HOME}/bin/clara-orchestrator",
+                "-t", "10",
+                "-i", "$CLAS12DIR/exp/input",
+                "custom services.yml",
+                "data/files.txt"
+                }));
+    }
+
+    @Test
+    public void outputStringNeedsQuotes() throws Exception {
+        assertThat(b.toString(), is(
+                "\"${CLARA_HOME}/bin/clara-orchestrator\""
+                + " -t 10"
+                + " -i \"$CLAS12DIR/exp/input\""
+                + " \"custom services.yml\""
+                + " data/files.txt"));
+    }
+
+    @Test
+    public void outputStringCanQuoteEverything() throws Exception {
+        b.quoteAll(true);
+
+        assertThat(b.toString(), is(
+                "\"${CLARA_HOME}/bin/clara-orchestrator\""
+                + " \"-t\" \"10\""
+                + " \"-i\" \"$CLAS12DIR/exp/input\""
+                + " \"custom services.yml\""
+                + " \"data/files.txt\""));
+    }
+}

commit ec8037aabd43755854980a8376a5556789906053

shell: fix typo in option to run-clara script
A space in the "-l" option makes the run-clara script fail due to
unrecognized option, because the new quoting behaviour introduced in
958613898d0b ("shell: improve quoting of external commands") adds quotes
to every argument with spaces, and before that the options were always
unquoted, which concealed the typo.

Closes #8.

src/org/jlab/clara/std/cli/FarmCommands.java
@@ -299,7 +299,7 @@ final class FarmCommands {
             cmd.addOption("-i", config.getString(Config.INPUT_DIR));
             cmd.addOption("-o", config.getString(Config.OUTPUT_DIR));
             if (config.hasValue(FARM_STAGE)) {
-                cmd.addOption("-l ", config.getString(FARM_STAGE));
+                cmd.addOption("-l", config.getString(FARM_STAGE));
             }
             if (config.hasValue(Config.MAX_THREADS)) {
                 cmd.addOption("-t", config.getInt(Config.MAX_THREADS));