07178b72cb48c97d219f2a9289cc4c8060e66fa0
gurjyan <gurjyan@jlab.org>
2019-08-01 14:56:53 -0400

    A simple watchdog as an ad-hoc solution for Clas12 stack jobs problem.

---
+++ scripts/unix/clas-watchdog.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# author Vardan Gyurjyan
+# date 7.27.19
+
+#################################
+# Parameters
+#################################
+
+# 1 = DPE process pid
+# 2 = Orchestrator process pid
+# 3 = timeout
+
+# Initialization
+dpe_pid=$1
+orch_pid=$2
+timeout=$3
+
+time=0
+cpu_idle="1.00"
+valid=true
+
+# main loop
+while [[ ${valid} ]]
+do
+
+# get cpu usage
+cpu_usage=`top -b -n 1 -p ${dpe_pid} | awk -v OFS="," '$1+0>0 { print $9 fflush() }'`
+
+echo $cpu_usage
+
+# check if dpe is done then exit.
+# This is the case when cpu_usage returns an empty string.
+if [[ -z ${cpu_usage} ]]; then
+exit 0
+fi
+
+# check if cpu_usage is less than 1%
+if [[ ${cpu_usage} < ${cpu_idle} ]] ; then
+time=$((time + 1))
+else
+time=0
+fi
+
+# check to see if we are not using CPU for timeout seconds.
+if (( $time > $timeout )); then
+kill -9 ${dpe_pid}
+kill -9 ${orch_pid}
+exit 1
+# create a new file.list and relaunch clara
+time=0
+fi
+
+sleep 10
+done
+
