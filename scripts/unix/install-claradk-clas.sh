5d8c5c08237dd7775538106102e607917c891c9d
gurjyan <gurjyan@jlab.org>
2019-08-01 15:57:52 -0400

    -update install script for clas12

--- scripts/unix/install-claradk-clas.sh
+++ scripts/unix/install-claradk-clas.sh
@@ -38,7 +38,6 @@ echo "Downloading and building xMsg package ..."
 git clone --depth 1 https://github.com/JeffersonLab/xmsg-java.git
 cd xmsg-java || exit
 ./gradlew install
-.gradlew deploy
 )
 
 (
