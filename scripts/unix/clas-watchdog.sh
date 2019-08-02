30c4ba68504bf24df044282a516ee3f771ad39a4
gurjyan <gurjyan@jlab.org>
2019-08-02 13:48:12 -0400

    - formatted clara-wd printouts

--- scripts/unix/clas-watchdog.sh
+++ scripts/unix/clas-watchdog.sh
@@ -26,7 +26,6 @@ do
 # get cpu usage
 cpu_usage=`top -b -n 1 -p ${dpe_pid} | awk -v OFS="," '$1+0>0 { print $9 fflush() }'`
 
-echo $cpu_usage
 
 # check if dpe is done then exit.
 # This is the case when cpu_usage returns an empty string.
@@ -36,6 +35,8 @@ fi
 
 # check if cpu_usage is less than 1%
 if [[ ${cpu_usage} < ${cpu_idle} ]] ; then
+echo `date`  "clara-wd:Error        DPE %CPU = ${cpu_usage} "
+
 time=$((time + 1))
 else
 time=0
@@ -43,6 +44,7 @@ fi
 
 # check to see if we are not using CPU for timeout seconds.
 if (( $time > $timeout )); then
+echo `date`  "clara-wd:SevereError  Stopped the data-processing... "
 kill -9 ${dpe_pid}
 kill -9 ${orch_pid}
 exit 1
